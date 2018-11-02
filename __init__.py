2afc4311194340e0427829e2eb617d4036d29d2d
Tiago Bilo <Tiago@UM-929LG8WM.local>
2018-11-02 11:21:15 -0400

    First commit

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+Vector Fields toolbox
+=============
+"""
+
+__authors__ = 'Tiago Bilo'
+__copyright__ = "left"
+__credits__ = "Tiago Bilo"
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Tiago Bilo"
+__email__ = "tiago.bilo@gmail.com"
+__status__ = "Beta"
+
+__all__ = "vector_fields"
+ 
+from psi_phi import *
